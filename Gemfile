5f0800648ffa340961276f3641b9d4049ba1da80
JP Ferreira <jpferreira@MA301.broadband>
2021-05-11 17:29:45 +0100

    Initial commit and push

---
+++ Gemfile
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+group :development do 
+  gem "rspec"
+  gem "sinatra"
+  gem "webrick"
+  gem "sinatra-contrib"
+  gem "capybara"
+  gem "selenium-webdriver"
+  gem "pry"
+  gem "chromedriver-helper"
+  gem "rexml"
+end
+
+# gem "rails"
--- app.rb
+++ app.rb
@@ -1,25 +1,41 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/' do
-    "Hello!"
+class Battle < Sinatra::Base
+    configure :development do
+        register Sinatra::Reloader
+    end
+
+    get "/" do
+        erb(:index)
+    end
+
+    run! if app_file == $0
 end
 
-get '/secret' do
-    "Hellooooooo?"
-end
+# get '/' do
+#     "Hello!"
+# end
 
-get '/page2' do
-    "Welcome to page 2"
-end
+# get '/secret' do
+#     "Hellooooooo?"
+# end
 
-get '/random-cat' do 
-    @name = ["Amigo", "Misty", "Almond"].sample
-    erb(:index)
-end
+# get '/page2' do
+#     "Welcome to page 2"
+# end
 
-get '/named-cat' do 
-    p params
-    @name = params[:name]
-    erb(:index)
-end 
+# get '/random-cat' do 
+#     @name = ["Amigo", "Misty", "Almond"].sample
+#     erb(:index)
+# end
+
+# post '/named-cat' do 
+#     p params
+#     @name = params[:name]
+#     erb(:index)
+# end 
+
+# get '/cat_form' do
+#     erb(:cat_form)
+# end
